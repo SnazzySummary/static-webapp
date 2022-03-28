9e65cba88eeb64829b7e6ead31ffc4d6078fa3c3
Robert Roe <robert.james.roe@gmail.com>
2022-03-28 16:35:47 -0700

    add React

---
+++ React/src/components/ToonList.js
@@ -0,0 +1,42 @@
+import React from 'react';
+//import toons from '../data/toons';
+import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
+const ToonList = (param) => {
+    const exceptId = param.exceptId;
+
+    // var others = toons;
+    // if (exceptId != undefined) {
+    //   others = toons.filter(p => p.id !== exceptId);
+    // } 
+
+    const [toonInfo, setToonInfo] = useState({});
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await fetch(`/api/toons`);
+            const body = await result.json();
+            setToonInfo(body);
+        }
+        fetchData();
+    }, []);
+
+    var others = toonInfo;
+
+    if (param !== undefined) {
+        others = Object.values(toonInfo).filter(p => p.id !== +param.exceptId);
+    }
+
+
+    return (
+        <>
+            {others.map((person, key) => (
+                <Link key={key} to={`/detail/${person.id}`}>
+                    <h6>{person.id} {person.firstName} {person.lastName}</h6>
+                </Link>
+            ))}
+        </>
+    )
+}
+export default ToonList; 
