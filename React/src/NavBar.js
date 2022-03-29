75613f54104b37e2d0d2bef14c239037126a6f95
Robert Roe <robert.james.roe@gmail.com>
2022-03-28 17:19:27 -0700

    fix

--- React/src/NavBar.js
+++ React/src/NavBar.js
@@ -10,9 +10,6 @@ const NavBar = () => (
                 <Link className="nav-link" to="/">Home</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/about">About</Link>
-            </li>
-            <li className="nav-item">
               <Link className="nav-link" to="/list">Toons List</Link>
             </li>
           </ul>
